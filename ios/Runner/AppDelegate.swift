3027db281efb7f24145e21c81218729cbe0343fd
Kuro fuji <Augusthne123@gmail.com>
2026-09-25 15:21:27 +0100

    git conflit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,11 +8,8 @@ import flutter_background_service_ios
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-<<<<<<< HEAD
-=======
     SwiftFlutterBackgroundServicePlugin.taskIdentifier = "dev.flutter.background.refresh"
     GeneratedPluginRegistrant.register(with: self)
->>>>>>> 5a625750440cdbf7e640b6bb2ed768397453a1ce
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
